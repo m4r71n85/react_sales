0a9e22125bbd58fa0fbabf8d6ee8dda069aa9308
Martin Tsekov <martin.tsekov.bg@gmail.com>
2019-12-28 13:57:59 +0200

    fix on react-bootstrap pkg

--- Sales/ClientApp/src/components/Home.js
+++ Sales/ClientApp/src/components/Home.js
@@ -1,55 +1,60 @@
 import React, { Component } from 'react';
+import Button from 'react-bootstrap/Button';
+
+
 
 export class Home extends Component {
     static displayName = Home.name;
 
-  constructor (props) {
-    super(props);
-    this.state = { forecasts: [], loading: true };
+    constructor(props) {
+        super(props);
+        this.state = { users: [], loading: true };
 
-    fetch('api/Users')
-      .then(response => response.json())
-      .then(data => {
-        this.setState({ forecasts: data, loading: false });
-      });
-  }
+        fetch('api/Users')
+            .then(response => response.json())
+            .then(data => {
+                this.setState({ users: data, loading: false });
+            });
+    }
 
-  static renderForecastsTable (forecasts) {
-    return (
-      <table className='table table-striped'>
-        <thead>
-          <tr>
-            <th>Date</th>
-            <th>Temp. (C)</th>
-            <th>Temp. (F)</th>
-            <th>Summary</th>
-          </tr>
-        </thead>
-        <tbody>
-          {forecasts.map(forecast =>
-            <tr key={forecast.userId}>
-              <td>{forecast.firstName}</td>
-              <td>{forecast.surname}</td>
-              <td>{forecast.email}</td>
-              <td>{forecast.summary}</td>
-            </tr>
-          )}
-        </tbody>
-      </table>
+    static renderUsersData(forecasts) {
+        return (
+            <table className='table table-striped'>
+                <thead>
+                    <tr>
+                        <th>First name</th>
+                        <th>Surname</th>
+                        <th>Email</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {forecasts.map(forecast =>
+                        <tr key={forecast.userId}>
+                            <td>{forecast.firstName}</td>
+                            <td>{forecast.surname}</td>
+                            <td>{forecast.email}</td>
+                        </tr>
+                    )}
+                </tbody>
+            </table>
+        );
+    }
+
+    render() {
+        let contents = this.state.loading
+            ? <p><em>Loading...</em></p>
+            : Home.renderUsersData(this.state.users);
+
+        return (
+
+
+            <div>
+                <Button variant="warning">Warning</Button>
+
+                <h1>Sales data</h1>
+                <p>This is an interview project for Client Savvy developed by Martin Tsekov.</p>
+                {contents}
+            </div>
     );
-  }
-
-  render () {
-    let contents = this.state.loading
-      ? <p><em>Loading...</em></p>
-        : Home.renderForecastsTable(this.state.forecasts);
-
-    return (
-      <div>
-        <h1>Sales data</h1>
-            <p>This is an interview project for Client Savvy developed by Martin Tsekov.</p> 
-        {contents}
-      </div>
-    );
-  }
+    }
 }
