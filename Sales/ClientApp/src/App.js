8896a2c94194a3422dce529f9746c0e59a0b4592
Martin Tsekov <martin.tsekov.bg@gmail.com>
2019-12-28 15:47:27 +0200

    work on remote pagination

--- Sales/ClientApp/src/App.js
+++ Sales/ClientApp/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Route } from 'react-router';
 import { Layout } from './components/Layout';
 import { Home } from './components/Home';
-import Button from 'react-bootstrap/Button';
+
 
 export default class App extends Component {
   static displayName = App.name;
--- Sales/ClientApp/src/components/Home.js
+++ Sales/ClientApp/src/components/Home.js
@@ -1,59 +1,89 @@
 import React, { Component } from 'react';
 import Button from 'react-bootstrap/Button';
+import paginationFactory from 'react-bootstrap-table2-paginator';
+import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
+import 'react-bootstrap-table2-paginator/dist/react-bootstrap-table2-paginator.min.css';
 
+// with es6
+import BootstrapTable from 'react-bootstrap-table-next';
 
 
 export class Home extends Component {
-    static displayName = Home.name;
+    static columns = [{
+        dataField: 'firstName',
+        text: 'FirstName',
+        sort: true
+    }, {
+        dataField: 'surname',
+        text: 'Surname',
+        sort: true
+    }, {
+        dataField: 'email',
+        text: 'Email',
+        sort: true
+    }];
 
     constructor(props) {
         super(props);
-        this.state = { users: [], loading: true };
-
-        fetch('api/Users')
-            .then(response => response.json())
-            .then(data => {
-                this.setState({ users: data, loading: false });
-            });
+        this.state = { users: [], loading: true, page: 1, sizePerPage: 10 };
+        this.products = [];
+        this.handleTableChange(null, {page: 1, sizePerPage: 10});
     }
 
-    static renderUsersData(forecasts) {
+
+    handleTableChange = (type, { page, sizePerPage }) => {
+        fetch('api/Users/' + sizePerPage + '/' + page)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data)
+                this.setState({
+                    page,
+                    users: data.usersData,
+                    sizePerPage,
+                    totalSize: data.totalSize,
+                    loading: false
+                });
+            });
+    }
+    
+
+    resetUsersData() {
+        console.log('resetting..');
+    }
+
+    static renderUsersData(users, page, sizePerPage, onTableChange, totalSize) {
         return (
-            <table className='table table-striped'>
-                <thead>
-                    <tr>
-                        <th>First name</th>
-                        <th>Surname</th>
-                        <th>Email</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {forecasts.map(forecast =>
-                        <tr key={forecast.userId}>
-                            <td>{forecast.firstName}</td>
-                            <td>{forecast.surname}</td>
-                            <td>{forecast.email}</td>
-                        </tr>
-                    )}
-                </tbody>
-            </table>
-        );
+            <BootstrapTable remote keyField='userId' data={users} columns={this.columns}
+                pagination={paginationFactory({ page, sizePerPage, totalSize })}
+                onTableChange={onTableChange} />
+        )
     }
 
     render() {
-        let contents = this.state.loading
+        let usersTableContent = this.state.loading
             ? <p><em>Loading...</em></p>
-            : Home.renderUsersData(this.state.users);
+            : Home.renderUsersData(this.state.users, this.state.page, this.state.sizePerPage, this.state.totalSize,
+            this.handleTableChange);
 
-        return (
-
-
-            <div>
-                <Button variant="warning">Warning</Button>
+    return (
+            <div className="mt-5">
 
                 <h1>Sales data</h1>
                 <p>This is an interview project for Client Savvy developed by Martin Tsekov.</p>
-                {contents}
+
+                <div className="row mb-2">
+                    <div className="col">
+                        <Button variant="warning" onClick={this.resetUsersData}>Reset Data</Button>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col">
+                        {usersTableContent}
+                    </div>
+                    <div className="col">
+                        ...
+                    </div>
+                </div>
             </div>
     );
     }
