be75cebbb2c85e38263679f35f7022ca8f47d075
Martin Tsekov <martin.tsekov.bg@gmail.com>
2019-12-28 18:23:09 +0200

    add rechart - plugin

--- Sales/ClientApp/src/components/Home.js
+++ Sales/ClientApp/src/components/Home.js
@@ -4,9 +4,8 @@ import paginationFactory from 'react-bootstrap-table2-paginator';
 import axios from 'axios';
 import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
 import 'react-bootstrap-table2-paginator/dist/react-bootstrap-table2-paginator.min.css';
-
-// with es6
 import BootstrapTable from 'react-bootstrap-table-next';
+import { BarChart, Bar, ReferenceLine, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 
 
 export class Home extends Component {
@@ -23,7 +22,51 @@ export class Home extends Component {
         dataField: 'email',
         text: 'Email',
         sort: true
-    }];
+        }];
+    data = [
+        {
+            "name": "Page A",
+            "uv": 4000,
+            "pv": 2400,
+            "amt": 2400
+        },
+        {
+            "name": "Page B",
+            "uv": 3000,
+            "pv": 1398,
+            "amt": 2210
+        },
+        {
+            "name": "Page C",
+            "uv": 2000,
+            "pv": 9800,
+            "amt": 2290
+        },
+        {
+            "name": "Page D",
+            "uv": 2780,
+            "pv": 3908,
+            "amt": 2000
+        },
+        {
+            "name": "Page E",
+            "uv": 1890,
+            "pv": 4800,
+            "amt": 2181
+        },
+        {
+            "name": "Page F",
+            "uv": 2390,
+            "pv": 3800,
+            "amt": 2500
+        },
+        {
+            "name": "Page G",
+            "uv": 3490,
+            "pv": 4300,
+            "amt": 2100
+        }
+    ]
 
     constructor(props) {
         super(props);
@@ -53,7 +96,7 @@ export class Home extends Component {
                     loading: false
                 });
             });
-    }    
+    }
 
     resetUsersData = () => {
         axios.post('api/reset')
@@ -71,23 +114,31 @@ export class Home extends Component {
         )
     }
 
+    static renderTopUsersChart() {
+        return (
+            <BarChart width={730} height={250} data={this.data}>
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Bar dataKey="pv" fill="#8884d8" />
+                <Bar dataKey="uv" fill="#82ca9d" />
+                <ReferenceLine y={4000} label="{this.data}" stroke="red" strokeDasharray="3 3" />
+            </BarChart>
+        );
+    }
+
 
 
     render() {
         let usersTableContent = Home.renderUsersData(this.state.users, this.state.page, this.state.sizePerPage, this.state.totalSize,
             this.handleTableChange);
 
+        let topUserSales = Home.renderTopUsersChart()
+
     return (
-            <div className="mt-5">
-
-            <div className="spinner">
-                <div className="rect1" />
-                <div className="rect2" />
-                <div className="rect3" />
-                <div className="rect4" />
-                <div className="rect5" />
-            </div>
-
+        <div className="mt-5">
                 <h1>Sales data</h1>
                 <p>This is an interview project for Client Savvy developed by Martin Tsekov.</p>
 
@@ -100,8 +151,9 @@ export class Home extends Component {
                     <div className="col-7">
                         {usersTableContent}
                     </div>
-                    <div className="col">
-                        ...
+                    <div className="col-5">
+                        {topUserSales}
+
                     </div>
                 </div>
             </div>
