e4aa8654a7667b0093a109783820f00f02786cb7
Martin Tsekov <martin.tsekov.bg@gmail.com>
2019-12-28 19:39:40 +0200

    fix on cell formatter loosing reference to function

--- Sales/ClientApp/src/components/Home.js
+++ Sales/ClientApp/src/components/Home.js
@@ -6,73 +6,86 @@ import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
 import 'react-bootstrap-table2-paginator/dist/react-bootstrap-table2-paginator.min.css';
 import BootstrapTable from 'react-bootstrap-table-next';
 import { BarChart, Bar, ReferenceLine, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
-
+import './Home.css';
 
 export class Home extends Component {
     pageSize = 10;
-    static columns = [{
+    columns = [{
         dataField: 'firstName',
         text: 'First Name',
-        sort: true
+        sort: true,
+        attrs: { width: 50, className: "EditRow" }
     }, {
         dataField: 'surname',
         text: 'Surname',
-        sort: true
+        sort: true,
+        attrs: { width: 50, className: "EditRow" }
     }, {
         dataField: 'email',
         text: 'Email',
-        sort: true
-        }];
+        sort: true,
+        attrs: { width: 50, className: "EditRow" }
+    }, {
+        dataField: "userId",
+        text: "Actions",
+        sort: false,
+            formatter: (cell, row, rowIndex) => {
+                console.log(cell);
+            return (
+                <Button variant="warning" onClick={this.resetUsersData}>Compare</Button>
+            );
+        },
+        headerAttrs: { width: 50 },
+        attrs: { width: 50, className: "EditRow" }
+    }];
     data = [
-        {
-            "name": "Page A",
-            "uv": 4000,
-            "pv": 2400,
-            "amt": 2400
-        },
-        {
-            "name": "Page B",
-            "uv": 3000,
-            "pv": 1398,
-            "amt": 2210
-        },
-        {
-            "name": "Page C",
-            "uv": 2000,
-            "pv": 9800,
-            "amt": 2290
-        },
-        {
-            "name": "Page D",
-            "uv": 2780,
-            "pv": 3908,
-            "amt": 2000
-        },
-        {
-            "name": "Page E",
-            "uv": 1890,
-            "pv": 4800,
-            "amt": 2181
-        },
-        {
-            "name": "Page F",
-            "uv": 2390,
-            "pv": 3800,
-            "amt": 2500
-        },
-        {
-            "name": "Page G",
-            "uv": 3490,
-            "pv": 4300,
-            "amt": 2100
-        }
-    ]
+            {
+                "name": "Page A",
+                "uv": 4000,
+            },
+            {
+                "name": "Page B",
+                "uv": 3000,
+            },
+            {
+                "name": "Page C",
+                "uv": 2000,
+            },
+            {
+                "name": "Page D",
+                "uv": 2780,
+            },
+            {
+                "name": "Page E",
+                "uv": 1890,
+            },
+            {
+                "name": "Page F",
+                "uv": 2390,
+            },
+            {
+                "name": "Page G",
+                "uv": 3490,
+            },
+            {
+                "name": "Page H",
+                "uv": 3490,
+            },
+            {
+                "name": "Page I",
+                "uv": 3490,
+            },
+            {
+                "name": "Page J",
+                "uv": 3490,
+            }
+        ];
 
     constructor(props) {
         super(props);
+
         this.state = { users: [], loading: true, page: 1, sizePerPage: this.pageSize };
-        this.products = [];
-        this.handleTableChange(null, { page: 1, sizePerPage: this.pageSize});
+        this.loadUsers(1);
     }
 
     handleTableChange = (type, { page, sizePerPage, filters, sortField, sortOrder }) => {
@@ -88,54 +101,51 @@ export class Home extends Component {
                 sortOrder
             }
         })
-            .then(response => {
-                this.setState({
-                    page,
-                    users: response.data.usersData,
-                    totalSize: response.data.totalSize,
-                    loading: false
-                });
+        .then(response => {
+            this.setState({
+                page,
+                users: response.data.usersData,
+                totalSize: response.data.totalSize,
+                loading: false
             });
+        });
     }
 
     resetUsersData = () => {
         axios.post('api/reset')
-            .then((resp) => {
-                this.loadUsers(1)
-            });
+            .then(() => {
+                    this.loadUsers(1);
+                });
     }
 
-    static renderUsersData(users, page, sizePerPage,  totalSize, onTableChange) {
+    static renderUsersData(users, page, sizePerPage, totalSize, onTableChange, columns) {
         return (
-            <BootstrapTable remote keyField='userId' data={users} columns={this.columns}
+            <BootstrapTable remote keyField='userId' data={users} columns={columns}
                 pagination={paginationFactory({ page, sizePerPage, totalSize, hideSizePerPage: true })}
                 noDataIndication={"Loading..."}
                 onTableChange={onTableChange} />
         )
     }
 
-    static renderTopUsersChart() {
+    static renderTopUsersChart(data, compareUser) {
         return (
-            <BarChart width={730} height={250} data={this.data}>
+            <BarChart width={640} height={250} data={data}>
                 <CartesianGrid strokeDasharray="3 3" />
                 <XAxis dataKey="name" />
                 <YAxis />
                 <Tooltip />
                 <Legend />
-                <Bar dataKey="pv" fill="#8884d8" />
-                <Bar dataKey="uv" fill="#82ca9d" />
-                <ReferenceLine y={4000} label="{this.data}" stroke="red" strokeDasharray="3 3" />
+                <Bar dataKey="uv" fill="#686dff" />
+                <ReferenceLine y={4000} label="adfdf" stroke="red" strokeDasharray="3 3" />
             </BarChart>
         );
     }
 
-
-
     render() {
         let usersTableContent = Home.renderUsersData(this.state.users, this.state.page, this.state.sizePerPage, this.state.totalSize,
-            this.handleTableChange);
+            this.handleTableChange, this.columns);
 
-        let topUserSales = Home.renderTopUsersChart()
+        let topUserSales = Home.renderTopUsersChart(this.data)
 
     return (
         <div className="mt-5">
@@ -148,12 +158,11 @@ export class Home extends Component {
                     </div>
                 </div>
                 <div className="row">
-                    <div className="col-7">
+                    <div className="col-6">
                         {usersTableContent}
                     </div>
-                    <div className="col-5">
+                    <div className="col-6">
                         {topUserSales}
-
                     </div>
                 </div>
             </div>
